d8831d6075baf5975f351980469250af94e60fbf
Rafael Motta <rafaelgmotta@gmail.com>
2016-12-12 14:32:44 -0200

    Implementação inicial do menu + timeline fragment + collection fragment

--- app/src/main/java/info/nsupdate/tboox/tboox/LoginActivity.java
+++ app/src/main/java/info/nsupdate/tboox/tboox/LoginActivity.java
@@ -3,6 +3,7 @@ package info.nsupdate.tboox.tboox;
 import android.animation.Animator;
 import android.animation.AnimatorListenerAdapter;
 import android.annotation.TargetApi;
+import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.support.annotation.NonNull;
 import android.support.design.widget.Snackbar;
@@ -92,6 +93,7 @@ public class LoginActivity extends AppCompatActivity implements LoaderCallbacks<
 
         mLoginFormView = findViewById(R.id.login_form);
         mProgressView = findViewById(R.id.login_progress);
+
     }
 
     private void populateAutoComplete() {
@@ -186,13 +188,15 @@ public class LoginActivity extends AppCompatActivity implements LoaderCallbacks<
             // perform the user login attempt.
             showProgress(true);
             mAuthTask = new UserLoginTask(email, password);
-            mAuthTask.execute((Void) null);
+            //mAuthTask.execute((Void) null);
+            startActivity(new Intent(LoginActivity.this, MenuActivity.class));
+            //mAuthTask.execute();
         }
     }
 
     private boolean isEmailValid(String email) {
         //TODO: Replace this with your own logic
-        return email.contains("@");
+        return true; //email.contains("@");
     }
 
     private boolean isPasswordValid(String password) {
