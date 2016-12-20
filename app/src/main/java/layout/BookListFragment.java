1aa9cccbefbd8730d3394080a65fdc428a29ca7a
Rafael Motta <rafaelgmotta@gmail.com>
2016-12-20 01:57:06 -0200

    Criação do BookList Fragment, onde serão exibidos os livros existentes no sistema

--- app/src/main/java/layout/BookListFragment.java
+++ app/src/main/java/layout/BookListFragment.java
@@ -4,14 +4,21 @@ import android.content.Context;
 import android.net.Uri;
 import android.os.Bundle;
 import android.app.Fragment;
-import android.support.design.widget.FloatingActionButton;
-import android.support.v4.app.FragmentManager;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.TextView;
+import android.widget.ArrayAdapter;
+import android.widget.ListAdapter;
+import android.widget.ListView;
+
 import com.loopj.android.http.JsonHttpResponseHandler;
+
+import org.json.JSONArray;
+import org.json.JSONException;
 import org.json.JSONObject;
+
+import java.util.ArrayList;
+
 import cz.msebera.android.httpclient.Header;
 import info.nsupdate.tboox.tboox.R;
 import info.nsupdate.tboox.tboox.utils.Services;
@@ -19,26 +26,25 @@ import info.nsupdate.tboox.tboox.utils.Services;
 /**
  * A simple {@link Fragment} subclass.
  * Activities that contain this fragment must implement the
- * {@link CollectionFragment.OnFragmentInteractionListener} interface
+ * {@link BookListFragment.OnFragmentInteractionListener} interface
  * to handle interaction events.
- * Use the {@link CollectionFragment#newInstance} factory method to
+ * Use the {@link BookListFragment#newInstance} factory method to
  * create an instance of this fragment.
  */
-public class CollectionFragment extends android.support.v4.app.Fragment{
-
+public class BookListFragment extends android.support.v4.app.Fragment {
+    // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
     private static final String ARG_PARAM1 = "param1";
     private static final String ARG_PARAM2 = "param2";
 
-
+    // TODO: Rename and change types of parameters
     private String mParam1;
     private String mParam2;
-    private static FloatingActionButton fabAdd;
 
     private OnFragmentInteractionListener mListener;
     private JSONObject data;
 
-    public CollectionFragment() {
+    public BookListFragment() {
         // Required empty public constructor
     }
 
@@ -48,18 +54,16 @@ public class CollectionFragment extends android.support.v4.app.Fragment{
      *
      * @param param1 Parameter 1.
      * @param param2 Parameter 2.
-     * @return A new instance of fragment CollectionFragment.
+     * @return A new instance of fragment BookListFragment.
      */
-
-    public static CollectionFragment newInstance(String param1, String param2) {
-        CollectionFragment fragment = new CollectionFragment();
+    // TODO: Rename and change types and number of parameters
+    public static BookListFragment newInstance(String param1, String param2) {
+        BookListFragment fragment = new BookListFragment();
         Bundle args = new Bundle();
         args.putString(ARG_PARAM1, param1);
         args.putString(ARG_PARAM2, param2);
         fragment.setArguments(args);
-
         return fragment;
-
     }
 
     @Override
@@ -75,7 +79,36 @@ public class CollectionFragment extends android.support.v4.app.Fragment{
             public void onSuccess(int statusCode, Header[] headers, JSONObject response) {
                 System.out.println("setting data!");
                 set_data(response);
-                show_data();
+
+                try {
+
+                    JSONObject jsonObject = new JSONObject(response.toString());
+
+                    JSONArray jsonArray = jsonObject.getJSONArray("data");
+                    int count=0;
+                    String uuid="", title="", synopsis="";
+                    ArrayList<String> test = new ArrayList<>();
+                    while (count<jsonArray.length())
+                    {
+                        JSONObject jo = jsonArray.getJSONObject(count);
+                        uuid = jo.getString("uuid");
+                        title = jo.getString("title");
+                        synopsis = jo.getString("synopsis");
+                        count++;
+                        String teste = uuid+" "+title+" "+synopsis;
+                        test.add(teste);
+                    }
+
+                    ListAdapter listAdapter = new ArrayAdapter<String>(getContext(), android.R.layout.simple_list_item_1,test);
+
+                    ListView bookListView = (ListView) getView().findViewById(R.id.lv_Books);
+
+                    bookListView.setAdapter(listAdapter);
+
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+
             }
 
             @Override
@@ -84,45 +117,17 @@ public class CollectionFragment extends android.support.v4.app.Fragment{
             }
         };
 
-        Services.get(this.getContext(), handler, "/collection");
-    }
-
-    private void show_data() {
-        String result = data.toString().substring(1, data.toString().length() - 1);
-
-        TextView data = (TextView) getView().findViewById(R.id.data);
-        data.setText(result);
+        Services.get(this.getContext(), handler, "/book");
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-
-        View v = inflater.inflate(R.layout.fragment_collection, container, false);
-
-        fabAdd = (FloatingActionButton) v.findViewById(R.id.floatingActionButtonAdd);
-        fabAdd.setOnClickListener(new View.OnClickListener()
-        {
-            @Override
-            public void onClick(View v)
-            {
-                BookListFragment bookListFragment = new BookListFragment();
-                //BookFragment bookFragment = new BookFragment();
-
-                FragmentManager fragmentManager = getFragmentManager();
-
-                fragmentManager.beginTransaction().replace(R.id.relative_content_menu,bookListFragment,bookListFragment.getTag()).commit();
-
-            }
-        });
-
-        return v;
-
-
+        return inflater.inflate(R.layout.fragment_book_list, container, false);
     }
 
-
+    // TODO: Rename method, update argument and hook method into UI event
     public void onButtonPressed(Uri uri) {
         if (mListener != null) {
             mListener.onFragmentInteraction(uri);
@@ -132,22 +137,20 @@ public class CollectionFragment extends android.support.v4.app.Fragment{
     @Override
     public void onAttach(Context context) {
         super.onAttach(context);
-        /*if (context instanceof OnFragmentInteractionListener) {
+        /*
+        if (context instanceof OnFragmentInteractionListener) {
             mListener = (OnFragmentInteractionListener) context;
         } else {
             throw new RuntimeException(context.toString()
                     + " must implement OnFragmentInteractionListener");
-        }*/
+        }
+        */
     }
 
     @Override
     public void onDetach() {
         super.onDetach();
         mListener = null;
-    }
-
-    public void set_data(JSONObject data) {
-        this.data = data;
     }
 
     /**
@@ -161,7 +164,10 @@ public class CollectionFragment extends android.support.v4.app.Fragment{
      * >Communicating with Other Fragments</a> for more information.
      */
     public interface OnFragmentInteractionListener {
-
+        // TODO: Update argument type and name
         void onFragmentInteraction(Uri uri);
+    }
+    public void set_data(JSONObject data) {
+        this.data = data;
     }
 }
