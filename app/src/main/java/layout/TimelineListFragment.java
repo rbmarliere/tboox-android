60e08beefeb847bd72ba348555de29332578a697
Ricardo Marliere <rbmarliere@gmail.com>
2016-12-21 01:47:23 -0200

    ReviewAdapter and TimelineListFragment

--- app/src/main/java/layout/TimelineListFragment.java
+++ app/src/main/java/layout/TimelineListFragment.java
@@ -4,91 +4,95 @@ import android.content.Context;
 import android.net.Uri;
 import android.os.Bundle;
 import android.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+
 import info.nsupdate.tboox.tboox.R;
+import info.nsupdate.tboox.tboox.adapters.ReviewAdapter;
+import info.nsupdate.tboox.tboox.models.Review;
+import info.nsupdate.tboox.tboox.utils.APIHandler;
+import info.nsupdate.tboox.tboox.utils.Services;
 
-/**
- * A simple {@link Fragment} subclass.
- * Activities that contain this fragment must implement the
- * {@link TimelineListFragment.OnFragmentInteractionListener} interface
- * to handle interaction events.
- * Use the {@link TimelineListFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
-public class TimelineListFragment extends android.support.v4.app.Fragment {
+public class TimelineListFragment extends android.support.v4.app.Fragment
+{
+    private TimelineListFragment.OnFragmentInteractionListener mListener;
+    private RecyclerView mRecyclerView;
+    private RecyclerView.Adapter mAdapter;
+    private RecyclerView.LayoutManager mLayoutManager;
 
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    private String mParam1;
-    private String mParam2;
-
-    private OnFragmentInteractionListener mListener;
-
-    public TimelineListFragment() {
+    public TimelineListFragment()
+    {
         // Required empty public constructor
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment TimelineListFragment.
-     */
-
-    public static TimelineListFragment newInstance(String param1, String param2) {
-        TimelineListFragment fragment = new TimelineListFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
-
     @Override
-    public void onCreate(Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
+
+        APIHandler handler = new APIHandler() {
+            @Override
+            public void handle_data(JSONObject response) {
+                try {
+                    JSONArray data_array = response.getJSONArray("data");
+
+                    ArrayList<Review> reviews = new ArrayList<>();
+                    for (int i = 0; i < response.length(); i++)
+                        reviews.add(new Review(data_array.getJSONObject(i)));
+
+                    mAdapter = new ReviewAdapter(reviews);
+                    mRecyclerView.setAdapter(mAdapter);
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+            }
+        };
+
+        Services.get(this.getContext(), handler, "/user/timeline");
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_timeline, container, false);
-    }
+                             Bundle savedInstanceState)
+    {
+        View v = inflater.inflate(R.layout.fragment_book_list, container, false);
 
-    public void onButtonPressed(Uri uri) {
-        if (mListener != null) {
-            mListener.onFragmentInteraction(uri);
-        }
+        mLayoutManager = new LinearLayoutManager(this.getContext());
+
+        mRecyclerView = (RecyclerView) v.findViewById(R.id.book_recycler_view);
+        mRecyclerView.setHasFixedSize(true);
+        mRecyclerView.setLayoutManager(mLayoutManager);
+
+        return v;
     }
 
     @Override
-    public void onAttach(Context context) {
+    public void onAttach(Context context)
+    {
         super.onAttach(context);
-        /*
-        if (context instanceof OnFragmentInteractionListener) {
-            mListener = (OnFragmentInteractionListener) context;
-        } else {
-            throw new RuntimeException(context.toString()
-                    + " must implement OnFragmentInteractionListener");
-        }*/
     }
 
     @Override
-    public void onDetach() {
+    public void onDetach()
+    {
         super.onDetach();
         mListener = null;
+    }
+
+    // TODO: Rename method, update argument and hook method into UI event
+    public void onButtonPressed(Uri uri)
+    {
+        if (mListener != null)
+            mListener.onFragmentInteraction(uri);
     }
 
     /**
@@ -101,8 +105,10 @@ public class TimelineListFragment extends android.support.v4.app.Fragment {
      * "http://developer.android.com/training/basics/fragments/communicating.html"
      * >Communicating with Other Fragments</a> for more information.
      */
-    public interface OnFragmentInteractionListener {
+    public interface OnFragmentInteractionListener
+    {
         // TODO: Update argument type and name
         void onFragmentInteraction(Uri uri);
     }
+
 }
