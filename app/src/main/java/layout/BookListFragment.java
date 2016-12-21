13f5984518dfe55387b8ed113032bf42ab81de60
Ricardo Marliere <rbmarliere@gmail.com>
2016-12-21 01:02:01 -0200

    cleaning BookDetailFragment -- UNTESTED

--- app/src/main/java/layout/BookListFragment.java
+++ app/src/main/java/layout/BookListFragment.java
@@ -63,8 +63,6 @@ public class BookListFragment extends android.support.v4.app.Fragment
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState)
     {
-        // Inflate the layout for this fragment
-
         View v = inflater.inflate(R.layout.fragment_book_list, container, false);
 
         mLayoutManager = new LinearLayoutManager(this.getContext());
