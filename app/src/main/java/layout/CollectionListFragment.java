c4ddc978d94b7982851c26c83cc7684345cda77c
Ricardo Marliere <rbmarliere@gmail.com>
2016-12-21 01:28:54 -0200

    CollectionListFragment and its layouts

--- app/src/main/java/layout/CollectionListFragment.java
+++ app/src/main/java/layout/CollectionListFragment.java
@@ -3,156 +3,97 @@ package layout;
 import android.content.Context;
 import android.net.Uri;
 import android.os.Bundle;
-import android.app.Fragment;
-import android.support.design.widget.FloatingActionButton;
-import android.support.v4.app.FragmentManager;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.TextView;
-import com.loopj.android.http.JsonHttpResponseHandler;
+
+import org.json.JSONArray;
+import org.json.JSONException;
 import org.json.JSONObject;
-import cz.msebera.android.httpclient.Header;
+
+import java.util.ArrayList;
+
 import info.nsupdate.tboox.tboox.R;
+import info.nsupdate.tboox.tboox.adapters.BookAdapter;
+import info.nsupdate.tboox.tboox.adapters.CollectionAdapter;
+import info.nsupdate.tboox.tboox.models.Book;
+import info.nsupdate.tboox.tboox.models.Collection;
+import info.nsupdate.tboox.tboox.utils.APIHandler;
 import info.nsupdate.tboox.tboox.utils.Services;
 
-/**
- * A simple {@link Fragment} subclass.
- * Activities that contain this fragment must implement the
- * {@link CollectionListFragment.OnFragmentInteractionListener} interface
- * to handle interaction events.
- * Use the {@link CollectionListFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
-public class CollectionListFragment extends android.support.v4.app.Fragment{
+public class CollectionListFragment extends android.support.v4.app.Fragment
+{
+    private CollectionListFragment.OnFragmentInteractionListener mListener;
+    private RecyclerView mRecyclerView;
+    private RecyclerView.Adapter mAdapter;
+    private RecyclerView.LayoutManager mLayoutManager;
 
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-
-    private String mParam1;
-    private String mParam2;
-    private static FloatingActionButton fabAdd;
-
-    private OnFragmentInteractionListener mListener;
-    private JSONObject data;
-
-    public CollectionListFragment() {
+    public CollectionListFragment()
+    {
         // Required empty public constructor
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment CollectionListFragment.
-     */
-
-    public static CollectionListFragment newInstance(String param1, String param2) {
-        CollectionListFragment fragment = new CollectionListFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-
-        return fragment;
-
-    }
-
     @Override
-    public void onCreate(Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
 
-        JsonHttpResponseHandler handler = new JsonHttpResponseHandler() {
+        APIHandler handler = new APIHandler() {
             @Override
-            public void onSuccess(int statusCode, Header[] headers, JSONObject response) {
-                System.out.println("setting data!");
-                set_data(response);
-                show_data();
-            }
+            public void handle_data(JSONObject response) {
+                try {
+                    JSONArray data_array = response.getJSONArray("data");
 
-            @Override
-            public void onFailure(int statusCode, Header[] headers, Throwable throwable, JSONObject errorResponse) {
-                System.out.println(errorResponse.toString());
-            }
+                    ArrayList<Collection> collection = new ArrayList<>();
+                    for (int i = 0; i < response.length(); i++)
+                        collection.add(new Collection(data_array.getJSONObject(i)));
 
-            @Override
-            public void onFailure(int statusCode, Header[] headers, String responseString, Throwable throwable) {
-                System.out.println(responseString);
+                    mAdapter = new CollectionAdapter(collection);
+                    mRecyclerView.setAdapter(mAdapter);
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
             }
         };
 
         Services.get(this.getContext(), handler, "/collection");
     }
 
-    private void show_data() {
-        String result = data.toString().substring(1, data.toString().length() - 1);
-
-        TextView data = (TextView) getView().findViewById(R.id.data);
-        data.setText(result);
-    }
-
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-
+                             Bundle savedInstanceState)
+    {
         View v = inflater.inflate(R.layout.fragment_collection_list, container, false);
 
-        fabAdd = (FloatingActionButton) v.findViewById(R.id.floatingActionButtonAdd);
-        fabAdd.setOnClickListener(new View.OnClickListener()
-        {
-            @Override
-            public void onClick(View v)
-            {
-                BookListFragment bookListFragment = new BookListFragment();
-                //BookDetailFragment bookFragment = new BookDetailFragment();
+        mLayoutManager = new LinearLayoutManager(this.getContext());
 
-                FragmentManager fragmentManager = getFragmentManager();
-
-                fragmentManager.beginTransaction().replace(R.id.relative_content_menu,bookListFragment,bookListFragment.getTag()).commit();
-
-            }
-        });
+        mRecyclerView = (RecyclerView) v.findViewById(R.id.collection_recycler_view);
+        mRecyclerView.setHasFixedSize(true);
+        mRecyclerView.setLayoutManager(mLayoutManager);
 
         return v;
-
-
-    }
-
-
-    public void onButtonPressed(Uri uri) {
-        if (mListener != null) {
-            mListener.onFragmentInteraction(uri);
-        }
     }
 
     @Override
-    public void onAttach(Context context) {
+    public void onAttach(Context context)
+    {
         super.onAttach(context);
-        /*if (context instanceof OnFragmentInteractionListener) {
-            mListener = (OnFragmentInteractionListener) context;
-        } else {
-            throw new RuntimeException(context.toString()
-                    + " must implement OnFragmentInteractionListener");
-        }*/
     }
 
     @Override
-    public void onDetach() {
+    public void onDetach()
+    {
         super.onDetach();
         mListener = null;
     }
 
-    public void set_data(JSONObject data) {
-        this.data = data;
+    // TODO: Rename method, update argument and hook method into UI event
+    public void onButtonPressed(Uri uri)
+    {
+        if (mListener != null)
+            mListener.onFragmentInteraction(uri);
     }
 
     /**
@@ -165,8 +106,10 @@ public class CollectionListFragment extends android.support.v4.app.Fragment{
      * "http://developer.android.com/training/basics/fragments/communicating.html"
      * >Communicating with Other Fragments</a> for more information.
      */
-    public interface OnFragmentInteractionListener {
-
+    public interface OnFragmentInteractionListener
+    {
+        // TODO: Update argument type and name
         void onFragmentInteraction(Uri uri);
     }
+
 }
