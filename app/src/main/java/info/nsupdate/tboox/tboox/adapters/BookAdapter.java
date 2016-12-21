397c43caaca84a91b86e98eb15472f7090dca66d
Ricardo Marliere <rbmarliere@gmail.com>
2016-12-21 01:28:10 -0200

    more cleaning

--- app/src/main/java/info/nsupdate/tboox/tboox/adapters/BookAdapter.java
+++ app/src/main/java/info/nsupdate/tboox/tboox/adapters/BookAdapter.java
@@ -23,12 +23,10 @@ public class BookAdapter extends RecyclerView.Adapter<BookAdapter.ViewHolder>
 
     @Override
     public BookAdapter.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
-        // create a new view
         View v = LayoutInflater.from(parent.getContext())
                 .inflate(R.layout.holder_book_list, parent, false);
-        // set the view's size, margins, paddings and layout parameters
-        //...
         ViewHolder vh = new ViewHolder(v);
+
         return vh;
 
     }
